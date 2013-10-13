9e6814c0da014e5e3e9ecc83a6a5ca5b5cdb34bd
ruski77 <adcowebsolutions@gmail.com>
2013-10-13 13:35:57 +1000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ConferenceTrackManagement::Application.config.secret_token = 'b2a415621a496eb1e9c04fbd29845c5ef2c6afacf00f76b2771571fa8f70a9a422ece8a3a42dfbc4b54dc77dc91c51da6fcfdc9c8f00890c3bc6942036058e9c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+ConferenceTrackManagement::Application.config.session_store :cookie_store, key: '_conference_track_management_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# ConferenceTrackManagement::Application.config.session_store :active_record_store
